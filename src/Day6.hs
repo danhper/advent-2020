85dca823d3ca7c861404f2501adaacb3079ff35f
Daniel Perez <daniel@perez.sh>
2020-12-06 14:02:20 +0000

    Add day 6

---
+++ src/Day6.hs
@@ -0,0 +1,33 @@
+module Day6 (
+  solve,
+) where
+
+import Data.List.Split (splitOn)
+import qualified Data.Map as M
+import Utils (formatIntResults)
+
+data Group = Group
+  { answers :: M.Map Char Int
+  , participants :: Int
+  }
+
+parseLines :: String -> [Group]
+parseLines content = map parseGroup groups
+ where
+  groups = splitOn [""] (lines content)
+  parseGroup group =
+    Group
+      { answers = foldr insert M.empty $ concat group
+      , participants = length group
+      }
+  insert c = M.insertWith (const (+ 1)) c 1
+
+countAnswers :: Group -> Int
+countAnswers group = length $ M.filter (== participants group) (answers group)
+
+solve :: String -> String
+solve content = formatIntResults part1 part2
+ where
+  groups = parseLines content
+  part1 = sum $ map (M.size . answers) groups
+  part2 = sum $ map countAnswers groups
--- src/Runner.hs
+++ src/Runner.hs
@@ -7,10 +7,11 @@ import qualified Day2
 import qualified Day3
 import qualified Day4
 import qualified Day5
+import qualified Day6
 import Utils (problemInputFile)
 
 solvers :: [String -> String]
-solvers = [Day1.solve, Day2.solve, Day3.solve, Day4.solve, Day5.solve]
+solvers = [Day1.solve, Day2.solve, Day3.solve, Day4.solve, Day5.solve, Day6.solve]
 
 run :: Int -> Maybe String -> IO String
 run n sample
